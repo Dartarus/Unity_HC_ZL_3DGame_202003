1e43ed7e92f93f64d95e6a2c985901a7d50012f7
Dartarus <62286996+Dartarus@users.noreply.github.com>
2020-03-26 21:47:54 +0800

    學習程式

--- 3DGame/Assets/Scripts/LearnCSharp.cs
+++ 3DGame/Assets/Scripts/LearnCSharp.cs
@@ -4,4 +4,22 @@ using UnityEngine;
 public class LearnCSharp : MonoBehaviour
 {
     //C#內的括號一定成對 () [] {} " "  ' '
+
+    //類別:成員
+    //1.屬性(欄位、屬性Prop)
+    //2.方法(功能)
+    //3.事件(Unity) - 特定時間執行的方法
+
+    //開始事件 : 播放遊戲時執行一次
+
+    private void Start()
+    {
+        print("嗨");
+    }
+
+    //更新事件:每一秒執行約60次
+    private void Update()
+    {
+        print("哈哈哈~");
+    }
 }
